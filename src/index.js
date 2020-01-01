ace642674e86f2281d535b29950d4a0f0f4da2a4
kwzm <931745526@qq.com>
2020-01-01 16:53:20 +0800

    add expirationTime demo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import List from "./List";
+import List from "./demos/fiber";
+import ExpirationTime from "./demos/expirationTime";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<List />, document.getElementById("root"));
+// ReactDOM.render(<List />, document.getElementById("root"));
+ReactDOM.render(<ExpirationTime />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
