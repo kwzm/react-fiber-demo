64521ccaeac70c802ac4691fbdc45a5003c59477
kwzm <931745526@qq.com>
2020-01-05 19:05:16 +0800

    add controlled component demo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import List from "./demos/fiber";
 import ExpirationTime from "./demos/expirationTime";
+import SetState from "./demos/setState";
+import ControlledComponent from "./demos/controlledComponent";
 import * as serviceWorker from "./serviceWorker";
 
-// ReactDOM.render(<List />, document.getElementById("root"));
-ReactDOM.render(<ExpirationTime />, document.getElementById("root"));
+ReactDOM.render(<List />, document.getElementById("list-container"));
+// ReactDOM.render(<ExpirationTime />, document.getElementById("root"));
+// ReactDOM.render(<SetState />, document.getElementById("root"));
+// ReactDOM.render(<ControlledComponent />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
