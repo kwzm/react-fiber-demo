1396202ef656fa3586c9dc9241418549471da81c
kwzm <931745526@qq.com>
2020-01-06 21:42:53 +0800

    add synthetic event demo

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import List from "./demos/fiber";
 import ExpirationTime from "./demos/expirationTime";
 import SetState from "./demos/setState";
 import ControlledComponent from "./demos/controlledComponent";
+import SyntheticEvent from "./demos/syntheticEvent";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<List />, document.getElementById("list-container"));
+// ReactDOM.render(<List />, document.getElementById("list-container"));
 // ReactDOM.render(<ExpirationTime />, document.getElementById("root"));
 // ReactDOM.render(<SetState />, document.getElementById("root"));
 // ReactDOM.render(<ControlledComponent />, document.getElementById("root"));
+ReactDOM.render(<SyntheticEvent />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
