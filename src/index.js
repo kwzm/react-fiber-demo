cb5a5b98553ee4728825b2c980d95a5d3849e61b
kwzm <931745526@qq.com>
2020-01-11 21:58:32 +0800

    add hooks demo

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import ExpirationTime from "./demos/expirationTime";
 import SetState from "./demos/setState";
 import ControlledComponent from "./demos/controlledComponent";
 import SyntheticEvent from "./demos/syntheticEvent";
+import UseStateDemo from "./demos/hooks/UseStateDemo";
 import * as serviceWorker from "./serviceWorker";
 
 // ReactDOM.render(<List />, document.getElementById("list-container"));
 // ReactDOM.render(<ExpirationTime />, document.getElementById("root"));
 // ReactDOM.render(<SetState />, document.getElementById("root"));
 // ReactDOM.render(<ControlledComponent />, document.getElementById("root"));
-ReactDOM.render(<SyntheticEvent />, document.getElementById("root"));
+// ReactDOM.render(<SyntheticEvent />, document.getElementById("root"));
+ReactDOM.render(<UseStateDemo />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
